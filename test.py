fffb6c51cc4c5e9ba421ec0d7e34fcc8cbd7418a
christhefr <43152835+christhefr@users.noreply.github.com>
2019-10-07 20:59:13 -0500

    update branch with my changes from local system

--- test.py
+++ test.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # Use text editor to edit the script and type in valid Instagram username/password
-
+import sys, json, os
+import requests
+import datetime
+import time
 from InstagramAPI import InstagramAPI
+
 
 def getTotalFollowers(api, user_id):
     """
     Returns the list of followers of the user.
     It should be equivalent of calling api.getTotalFollowers from InstagramAPI
     """
-
     followers = []
     next_max_id = True
     while next_max_id:
@@ -18,45 +21,71 @@ def getTotalFollowers(api, user_id):
         if next_max_id is True:
             next_max_id = ''
 
-        _ = api.getUserFollowers(user_id, maxid=next_max_id)
+        _= api.getUserFollowers(user_id, maxid=next_max_id)
         followers.extend(api.LastJson.get('users', []))
         next_max_id = api.LastJson.get('next_max_id', '')
     return followers
+
+def get_id(username):
+    url = "https://www.instagram.com/web/search/topsearch/?context=blended&query="+username+"&rank_token=0.3953592318270893&count=1"
+    response = requests.get(url)
+    respJSON = response.json()
+    try:
+	    user_id = str( respJSON['users'][0].get("user").get("pk") )
+	    return user_id
+    except:
+	    return "Unexpected error"
+
 
 api = InstagramAPI("iamjumpmanofficial", "Trilogeekd7!")
 if (api.login()):
     api.getSelfUserFeed()  # get self user feed
     print("\r\nLogin success!")
 
-    s = str(api.LastJson)
+    s = open("C:/Users/v784707/Downloads/PromoEngine-master/HiphopSearch.txt", "r")
+    s = str(s.read())
 
-    idx1 = 0
-    idx2 = -1
-    idx1 = s.find("@", idx1)
-    while idx1 > 0:
-        idx2 = s.find(" ", idx1)
-        #print("      idx1= " + str(idx1) + ", idx2= " + str(idx2))
-        s2 = s[idx1:idx2]
-        print(s2)
-        idx1 = s.find("@", idx2)
+    # build user list from instagram search or web scrape
+    count = 0 # count users found in search
+    #users = ['christhefr']
+    users = [ t for t in s.split() if t.startswith('@') ]
 
     print("\r\nWriting output file...")
-    f = open("Instagram-output.txt", "w", encoding='utf-8')
-    f.write(s)
+    f = open("Instagram-output.txt", "a", encoding='utf-8')
+    
+    for user in users:
+        # remove all characters starting at backslashes
+        result = user.find("\\")
+        if result > 0:
+            remchar = user[result:len(user)]
+            user = user.replace(remchar,"")
+            
+        # remove all characters starting at "
+        result = user.find('\"')
+        if result > 0:
+            remchar = user[result:len(user)]
+            user = user.replace(remchar,"")
+
+        # if user only has @ sign then remove
+        if len(user) != 1:
+            mediaId = '2139085373926303794_7932030313'	# a media_id
+            userid = get_id(user)
+            recipients = [userid]			# array of user_ids. They can be strings or ints
+            msg = 'Whats good.. I\'ve been searching the internet all day for people that might like my video. Please take a look when you have chance and feedback would be appreciated. Thx JumpMan'
+
+            try:
+                api.direct_share(mediaId, recipients, text=msg)
+                s = str(datetime.datetime.now().ctime()) + "," + user + "," + get_id(user) + ",True \n"
+                f.write(s)
+            except:
+                print("Limit reached, bot needs to sleep for 5 minutes.")
+                # Wait for 5 minutes
+                time.sleep(300)                    
+            print("Sending message to username: " + user + " userid: " + userid)
+            count = count + 1
+
     f.close()
-    # print(api.LastJson)  # print last response JSON
+    print("Found..." + str(count) + " users in search.")
 
-    # user_id = 15503147	# serenawilliams
-    user_id = 3280631674	# christhefr 
-    followers = getTotalFollowers(api, user_id)
-    print('Number of followers:', len(followers))
-
-    #next_max_id = ''
-    #followersB = api.getUserFollowers(1623112898, next_max_id)
-    #print('Number of followers:', len(followersB))
-
-    mediaId = '2139085373926303794_7932030313'	# a media_id
-    recipients = [1623112898]			# array of user_ids. They can be strings or ints
-    #api.direct_share(mediaId, recipients, text='iamjumpmanofficial')
 else:
     print("\r\nLogin failed!")
